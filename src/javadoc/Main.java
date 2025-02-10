50367992a98c1a5380e0833ccfd4c0fd4df8edc1
EnzoCordovana <106628886+EnzoCordovana@users.noreply.github.com>
2025-02-10 21:15:08 +0000

    Deploying to gh-pages from @ EnzoCordovana/JavaDoc@4fc3806d4b8d39a4a65b518c3bdc414aa02dbc76 🚀

--- src/javadoc/Main.java
+++
@@ -1,9 +0,0 @@
-package javadoc;
-
-public class Main {
-	public static void main(String[] args) {
-		Account myAccount = new Account(57, "BNK", "enzo.cordovana@etu.univ-amu.fr");
-		System.out.println(myAccount.getEmail());
-		System.out.println(myAccount);
-	}
-}
